508ed2cc6472bd2b3b2bee92bca947b78a0dd60a
eunj <dmswjd362@gmail.com>
2014-04-11 03:08:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 $(document).read(function(){
-  $('.A').click(function(){
-    $(this).toggleClass('.Aback');
-  });
-});
-    
+ var Y = function(){
+    if(count == 2){
+      count=0;
+      	if(savepoint[0] == savepoint[1]){
+      		checkpoint++;
+      	}
+      	eles{
+      		$('.A'+savepoint2[0].toggleClass('Aback'+savepoint[0]));
+      		$('.A'+savepoint2[0].toggleClass('Aback'+savepoint[0]));
+      	}
+    }
+ }
+ 	$('.A').click(function(){
+ 		$(this).toggleClass('Aback');
+ 		savepoint[count] = 1;
+ 		savepoint2[count] =1;
+ 		count +=1;
+ 		Y();
+ 	});
