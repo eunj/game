90a554057925eae034073e7175e2987488b2668e
eunj <dmswjd362@gmail.com>
2014-04-13 11:25:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 $(document).ready(function() {
-  $('card A').click(function(){
+  $('#A').click(function(){
     $(this).toggleClass('backA');
   });
 });
-$('card B').click(function(){
+$('#B').click(function(){
     $(this).toggleClass('backC');
   });
   
-$('card C').click(function(){
+$('#C').click(function(){
     $(this).toggleClass('backE');
   }); 
   
-$('card D').click(function(){
+$('#D').click(function(){
     $(this).toggleClass('backC');
   });  
   
-$('card E').click(function(){
+$('#E').click(function(){
     $(this).toggleClass('backB');
   });
   
-$('card F').click(function(){
+$('#F').click(function(){
     $(this).toggleClass('backA');
   });
   
-$('card G').click(function(){
+$('#G').click(function(){
     $(this).toggleClass('backD');
   });
   
-$('card H').click(function(){
+$('#H').click(function(){
     $(this).toggleClass('backB');
   });
   
-$('card I').click(function(){
+$('#I').click(function(){
     $(this).toggleClass('backD');
   });  
+  
+$('#J').click(function(){
+    $(this).toggleClass('backE');
+  });  
+  
+$('#K').click(function(){
+    $(this).toggleClass('backF');
+  });  
+  
+$('#L').click(function(){
+    $(this).toggleClass('backF');
+  });    
+  
+    
+  
+
